111479745578bdf8af4ada0006e21e9ae09a1d60
JuliaDSolano <juliaduartesolano@gmail.com>
2024-12-08 19:34:08 -0300

    feat: Adiciona relacionamento de Livro com Categoria e Editora

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,8 +1,14 @@
 from django.db import models
-
+from .editora import Editora
+from .categoria import Categoria
+    
 class Livro(models.Model):
-    nome = models.CharField(max_length=100)
-    site = models.URLField(max_length=200, blank=True, null=True)
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField(default=0,  null=True, blank=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
 
     def __str__(self):
-        return self.nome
+        return f"({self.id}) {self.titulo} ({self.quantidade})"
